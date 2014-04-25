b4f4c3ae6380581f3c3d2a0418ce55046cd3fbf8
darkwhite318 <darkwhite318@gmail.com>
2014-04-25 11:29:17 +0800

    add annotation

--- feature_match.m
+++ feature_match.m
@@ -8,6 +8,7 @@
 %   
 %========================================================================
 function O = feature_match(I1,list1,I2,list2)
+%set feature using gray image
 
 Num1 = size(list1,2);%Num1: number of feature points in I1
 Num2 = size(list2,2);%NUm2: number of feature points in I2
@@ -15,14 +16,14 @@ dis = ones(Num1,2);%the smallest distance between each feature point in I1 to I2
 Img1 = 54/256*I1(:,:,1)+183/256*I1(:,:,2)+19/256*I1(:,:,3);
 Img2 = 54/256*I2(:,:,1)+183/256*I2(:,:,2)+19/256*I2(:,:,3);
 
-
-w = 8;%widow size = 2*2+1 = 5
+%%
+%find the best corespoonding feature point for I1 and I2
+w = 8;%widow size = 8*8+1 = 17
 
 initM2 = Img2(list2(1,1)-w:list2(1,1)+w,list2(2,1)-w:list2(2,1)+w);
 for i = 1:Num1
     diM1 = Img1(list1(1,i)-w:list1(1,i)+w,list1(2,i)-w:list1(2,i)+w);
-    dis(i,1) = sum(sum((double(diM1) - double(initM2)).^2));
-    %dis(i,2) = 1;
+    dis(i,1) = sum(sum((double(diM1) - double(initM2)).^2));%score
     for j = 2:Num2        
         diM2 = Img2(list2(1,j)-w:list2(1,j)+w,list2(2,j)-w:list2(2,j)+w);
         p = double((double(diM1)-double(diM2)).^2);
@@ -33,31 +34,22 @@ for i = 1:Num1
           end
     end
 end
-
-% set theshold
-fdis = zeros(1,2);%a list of good feature point of I1 and I2
+%%
+% sort the coresponding score
+fdis = zeros(1,2);%a list of good feature point of I1 and I2 
 %nu = 1;
-[so turn] = sort(dis(:,1))
-nu = 20;
+[so turn] = sort(dis(:,1));
+if(size(dis(:,1),1) < 20)
+    nu = size(dis(:,1),1);
+else
+nu = 20; %the best 20 feature
+end
 for i = 1:nu
-    fdis(i,1) = turn(i,1);%order of image I2
-    fdis(i,2) = dis(turn(i,1),2);%order of image I1
-end
-%{
-for i = 1:Num1
-    if (dis(i,1) <threshold)
-        fdis(nu,1) = i;
-        fdis(nu,2) = dis(i,2);
-        nu = nu + 1;
-    end 
-end
-%}
-%if(nu ==1)
-%    disp('the threshold is too small!!');
-%    O = 0;
-%else
-    
+    fdis(i,1) = turn(i,1);%order of image I1's feature points in list1
+    fdis(i,2) = dis(turn(i,1),2);%order of image I2's feature points in list2
+end   
 O = dis;
+%%
 %show
 Ic = size(I1(:,:,1),2);
 num = size(fdis,1);
